2365664b60bab8e2154753023448e191cd13ccf7
12downsk <12downks@gmail.com>
2015-04-03 09:59:14 -0500

    Function to check for correct paren usage

--- parser.cpp
+++ parser.cpp
@@ -11,3 +11,21 @@ bool parse_ErrorCheck(){ //Checks for all errors that could occur while parsing
 
 	return false;
 }
+
+bool Parser::closing_paren(string exp)
+{
+	int open = 0;
+	int close = 0;
+
+	for (int i = 0; i < exp.size(); i++)
+	{
+		if (exp[i] == '(')
+			open++;
+		else if (exp[i] == ')')
+			close++;
+	}
+	if (open == close)
+		return true;
+	else
+		return false;
+}
